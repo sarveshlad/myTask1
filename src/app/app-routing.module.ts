0495626d974d289abe6ef1b8a78d0594b114c49a
sarveshlad <81073296+sarveshlad@users.noreply.github.com>
2021-09-17 21:19:57 +0530

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddtaskComponent } from './addtask/addtask.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+
+import { ShowtaskComponent } from './showtask/showtask.component';
+
+const routes: Routes = [ 
+ 
+  { path: 'addtask', component: AddtaskComponent },
+{ path: 'showtask', component:ShowtaskComponent },
+{ path:'edit-task/:titleId', component:EditTaskComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
